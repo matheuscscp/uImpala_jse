90fbccf1391e0c35f41e0f62c4a22b4299aa8263
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2014-06-26 15:44:42 -0300

    Fixed coordinate system used to draw shapes

--- src/org/unbiquitous/uImpala/jse/util/shapes/Rectangle.java
+++ src/org/unbiquitous/uImpala/jse/util/shapes/Rectangle.java
@@ -8,6 +8,7 @@ import org.lwjgl.opengl.GL11;
 public class Rectangle extends Shape {
 	private final float width;
 	private final float height;
+	private float angleInDegrees = 0;
 	
 	public Rectangle(Point center, Color paint, float width, float height) {
 		super(center, paint);
@@ -15,16 +16,21 @@ public class Rectangle extends Shape {
 		this.height = height;
 	}
 
+	public void rotate(float angleInDegrees) {
+		this.angleInDegrees = angleInDegrees;
+	}
+	
 	public void render(){
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 		paint.bind();
 		GL11.glLoadIdentity();
+		GL11.glTranslatef(center.x, center.y, 0.0f);
+		GL11.glRotatef(angleInDegrees, 0.0f, 0.0f, 1.0f);
 		GL11.glBegin(GL11.GL_POLYGON);
-		GL11.glVertex2d(center.x - width/2,center.y - height/2);
-		GL11.glVertex2d(center.x + width/2,center.y - height/2);
-		GL11.glVertex2d(center.x + width/2,center.y + height/2);
-		GL11.glVertex2d(center.x - width/2,center.y + height/2);
-		
+			GL11.glVertex2d(- width/2, - height/2);
+			GL11.glVertex2d(+ width/2, - height/2);
+			GL11.glVertex2d(+ width/2, + height/2);
+			GL11.glVertex2d(- width/2, + height/2);
 		GL11.glEnd();
 	}
 	
--- src/org/unbiquitous/uImpala/jse/util/shapes/SimetricShape.java
+++ src/org/unbiquitous/uImpala/jse/util/shapes/SimetricShape.java
@@ -8,6 +8,7 @@ import org.lwjgl.opengl.GL11;
 public class SimetricShape extends Shape {
 	float radius;
 	int sides;
+	private float angleInDegrees;
 	
 	public SimetricShape(Point center, Color paint, float radius, int sides) {
 		super(center, paint);
@@ -19,22 +20,28 @@ public class SimetricShape extends Shape {
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 		paint.bind();
 		GL11.glLoadIdentity();
+		GL11.glTranslatef(center.x, center.y, 0.0f);
+		GL11.glRotatef(angleInDegrees, 0.0f, 0.0f, 1.0f);
 		GL11.glBegin(GL11.GL_POLYGON);
-		renderCIrcle();
+		renderPoints();
 		GL11.glEnd();
 	}
 
-	private void renderCIrcle() {
+	private void renderPoints() {
 		for (double i = 0; i < 360; i+=((float)360)/sides) {
-			plotVertex(center, radius, i);
+			plotVertex(center, ((float)radius)/2, i+45);
 		}
 	}
 
-	private void plotVertex(Point center, float radius, double degrees) {
+	private void plotVertex(Point center, double radius, double degrees) {
 		double degInRad = Math.toRadians(degrees);
-		double x = center.x + Math.cos(degInRad) * radius;
-		double y = center.y + Math.sin(degInRad) * radius;
+		double x = Math.cos(degInRad) * radius;
+		double y = Math.sin(degInRad) * radius;
 		GL11.glVertex2d(x,y);
+	}
+	
+	public void rotate(float angleInDegrees) {
+		this.angleInDegrees = angleInDegrees;
 	}
 	
 	public float radius(){	return radius;	}
